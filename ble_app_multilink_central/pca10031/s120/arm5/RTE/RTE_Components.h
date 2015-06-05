bdb456501a4b7320f026189ae216dd29a8b244a9
Jithin <jlajithin@gmail.com>
2015-06-05 21:47:51 +0530

    [WIP] recieves data from RFduino

---
+++ ble_app_multilink_central/pca10031/s120/arm5/RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_multilink_central_s120_pca10031' 
+ * Target:  'nrf51422_xxac_s120' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define BSP_DEFINES_ONLY
+#define S120
+#define SOFTDEVICE_PRESENT
+
+#endif /* RTE_COMPONENTS_H */
